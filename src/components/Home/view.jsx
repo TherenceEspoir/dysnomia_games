b492ab4a61fff7c1cd01df2cffffbfa88283a2dc
Julian POPIEUL <julian.popieul@gmail.com>
2024-05-09 22:12:54 +0200

    barre de recherche (close #10)

--- src/components/Home/view.jsx
+++ src/components/Home/view.jsx
@@ -1,16 +1,23 @@
 import './index.css' ;
 
-export default function Top({best10}) {
-    
+export default function Results({movies, handleChange, title}){
+    let myvalue = localStorage.getItem("searchWords") ;
+    // console.log("en vue : ") ;
+    // console.log(movies) ;
+
     return (
         <div className='w-75 m-auto'>
-            <h1 className='text-center mb-4'>Top 10</h1>
+            <div className="mb-3 w-75 m-auto">
+                <input type='text' defaultValue={myvalue} placeholder="Search... (minimum 4 caractères)" id='search' name='search' className='form-control' onChange={handleChange}/>
+            </div>
+
+            <h1 className='text-center mb-4'>{title}</h1>
             
             
-            {best10.map((game) => (
+            {movies.map((game) => (
                 <div key={game.id} className='row'>
                     <div className='col-6 text-end mb-3'><a href={`/game/${game.id}`} className='aGame'>{game.name}</a></div>
-                    <div className='col-6'>{(game.rating).toFixed(3)}</div>
+                    <div className='col-6'>{(game.rating || NaN).toFixed(3)}</div>
                 </div>
             ))}
 
